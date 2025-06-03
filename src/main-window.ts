7a52b8b8160cfa91639be4654a714e609970e825
[nyx] <nyx_lyb3ra@noreply.codeberg.org>
2025-06-03 08:59:13 -0300

    rename project to k’uychi
    replaces the generic template name "GNOME TypeScript Template" with the
    actual application name "K’uychi".
    
    updates the application id, command, name, log domain, gobject type
    names, and urls across various configuration and source files.

--- src/main-window.ts
+++ src/main-window.ts
@@ -1,14 +1,13 @@
 import Adw from "gi://Adw";
 import Gio from "gi://Gio";
+import GLib from "gi://GLib";
 import GObject from "gi://GObject";
 
 import {gettext as _} from "gettext";
 
 import Template from "./main-window.ui";
-import GLib from "gi://GLib";
 
-// FIXME: Choose a namespace that fits your app
-const options = {GTypeName: "ExampleMainWindow", Template};
+const options = {GTypeName: "KuychiMainWindow", Template};
 
 class MainWindow extends Adw.ApplicationWindow {
   private readonly _settings = Gio.Settings.new(Config.APP_ID);
@@ -45,15 +44,13 @@ class MainWindow extends Adw.ApplicationWindow {
   }
 
   private showAboutDialog(): void {
-    // FIXME: Update the info here to something that fits your project
-
     const dialog = new Adw.AboutDialog({
       applicationIcon: Config.APP_ID,
       applicationName: GLib.get_application_name() ?? "",
       copyright: "© 2025 [nyx]",
       developerName: "[nyx]",
       developers: ["[nyx] https://nyx.liber.ar/"],
-      issueUrl: "https://codeberg.org/nyx_lyb3ra/gnome-ts-template/issues",
+      issueUrl: "https://codeberg.org/nyx_lyb3ra/kuychi/issues",
       license: _(
         "This application comes with absolutely no warranty. See the <a href='https://blueoakcouncil.org/license/1.0.0'>Blue Oak Model License 1.0.0</a> for details.",
       ),
