24cbfe9176a346c0f7500a61faaf7f9e2d1e7c11
[nyx] <nyx_lyb3ra@noreply.codeberg.org>
2025-06-08 23:15:33 -0300

    use coords for srgb components
    extracts the srgb color components using the `.coords` property, which
    provides the numerical coordinate values directly as an array

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -3,7 +3,7 @@ import Gdk from "gi://Gdk";
 import Color from "colorjs.io";
 
 export function colorToRgba(color: Color): Gdk.RGBA {
-  const {r: red, g: green, b: blue} = color.to("sRGB");
+  const [red, green, blue] = color.to("sRGB").coords;
   return new Gdk.RGBA({red, green, blue, alpha: 1});
 }
 
